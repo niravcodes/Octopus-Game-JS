e8f9f3e928337b6d1cbaa9781b7783977a647524
Nirav <niravnikhil@gmail.com>
2020-09-14 16:48:30 +0545

    1st ver#4

--- testGame/backgrounds/backBG.js
+++ testGame/backgrounds/backBG.js
@@ -1,5 +1,7 @@
-import Background from "../../library/background.js"
+import Background from "../../library/background.js";
 
-let bg = new Background(["/assets/underwater-fantasy-files/PNG/layers/sand.png"])
+let bg = new Background([
+  "assets/underwater-fantasy-files/PNG/layers/sand.png",
+]);
 bg.parallaxFactor = 0.25;
 export default bg;
--- testGame/backgrounds/midBG.js
+++ testGame/backgrounds/midBG.js
@@ -1,6 +1,9 @@
-import Background from "../../library/background.js"
+import Background from "../../library/background.js";
 
-let midBG = new Background(["/assets/underwater-fantasy-files/PNG/layers/foreground-2.png", "/assets/underwater-fantasy-files/PNG/layers/foreground-1.png"])
+let midBG = new Background([
+  "assets/underwater-fantasy-files/PNG/layers/foreground-2.png",
+  "/assets/underwater-fantasy-files/PNG/layers/foreground-1.png",
+]);
 // midBG.parallaxFactor = 0.7;
 midBG.parallaxFactor = 1;
 midBG.debugmode = true;
