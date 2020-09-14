4769e5ff5e9beb74ad180d843cc408f60a2701d4
Nirav <niravnikhil@gmail.com>
2020-09-14 15:53:12 +0545

    set up publication

---
+++ build.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+echo "Rebuilding Game"
+rm -rf dist
+mkdir -p dist/js
+cp index.html dist/index.html
+cp favicon.ico dist/favicon.ico
+
+echo "Copying Assets"
+cp -r assets dist/
+
+echo "Transpiling with Babel"
+npx babel library -d dist/js/library
+npx babel testGame -d dist/js/game
+
+echo "Browserifying"
+npx browserify dist/js/game/init.js -o dist/game.js
+
+echo "Cleaning up"
+rm -rf dist/js
+
+echo "Minifying"
+npx google-closure-compiler --js=dist/game.js --js_output_file=dist/gameM.js --jscomp_off=checkVars
+
+echo "Done! (ignore the warnings)"
